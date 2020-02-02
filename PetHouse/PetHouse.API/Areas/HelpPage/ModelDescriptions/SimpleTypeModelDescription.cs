aebb2a5fe8177625ae5fd1a353db4b35ca881342
dabakura <38744169+dabakura@users.noreply.github.com>
2020-02-02 02:02:35 -0600

    Initial commit

---
+++ PetHouse/PetHouse.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PetHouse.API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ PetHouse/PetHouse.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PetHouse.API.WebApiApplication" Language="C#" %>
